790792450800ff3108b374af8ab8d11d85d70e5a
Mariusz Cichy <mariusz.cichy@mcpm.pl>
2019-01-21 10:56:48 +0100

    Initial commit.

---
+++ src/main/java/pl/mcpm/gittestjeden/GitTestJedenApplication.java
@@ -0,0 +1,14 @@
+package pl.mcpm.gittestjeden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestJedenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestJedenApplication.class, args);
+	}
+
+}
+
